1b565f90b8db69b643b68a456cb683d7517c794f
Leo G <leoko4433@gmail.com>
2016-08-19 12:27:58 +0200

    Fixed .gitignore & changed plugin version to 2.0.5

---
+++ src/me/leoko/advancedban/BukkitMain.java
@@ -0,0 +1,46 @@
+package me.leoko.advancedban;
+
+import me.leoko.advancedban.listener.ChatListener;
+import me.leoko.advancedban.listener.CommandListener;
+import me.leoko.advancedban.listener.ConnectionListener;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+
+public class BukkitMain extends JavaPlugin {
+	private static BukkitMain instance;
+	public static BukkitMain get(){
+		return instance;
+	}
+
+	public void onEnable(){
+		instance = this;
+		Universal.get().setup(new BukkitMethods());
+
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+			System.out.println("[AdvancedBan] MC-Stats >> Connected");
+		} catch (IOException e) {
+			System.out.println("[AdvancedBan] Failed to send Stats!\n Contact: Leoko4433@gmail.com \n Error Code: AB344");
+		}
+
+		ConnectionListener connListener = new ConnectionListener();
+		this.getServer().getPluginManager().registerEvents(connListener, this);
+		this.getServer().getPluginManager().registerEvents(new ChatListener(), this);
+		this.getServer().getPluginManager().registerEvents(new CommandListener(), this);
+
+		for(Player op : Bukkit.getOnlinePlayers()){
+			AsyncPlayerPreLoginEvent apple = new AsyncPlayerPreLoginEvent(op.getName(), op.getAddress().getAddress(), op.getUniqueId());
+			connListener.onConnect(apple);
+			if(apple.getLoginResult() == AsyncPlayerPreLoginEvent.Result.KICK_BANNED) op.kickPlayer(apple.getKickMessage());
+		}
+	}
+	
+	public void onDisable() {
+		Universal.get().shutdown();
+	}
+}
