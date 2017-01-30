67f3161227a96d8b3ed4ed09bb6be3341e24e46f
Leo G <leoko4433@gmail.com>
2017-01-30 14:45:41 +0100

    [~] Fixed imports [~] not compile-able code [~] Changed code-style [-] '+' Char at beginning of every new line

--- src/me/leoko/advancedban/bungee/listener/CommandReceiverBungee.java
+++ src/me/leoko/advancedban/bungee/listener/CommandReceiverBungee.java
@@ -6,6 +6,9 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by Leoko @ dev.skamps.eu on 24.07.2016.
  */
@@ -23,17 +26,14 @@ public class CommandReceiverBungee extends Command implements TabExecutor{
     @Override
     public Iterable<String> onTabComplete(CommandSender commandSender, String[] strings) {
         Set<String> matches = new HashSet<>();
-+        if ( args.length == 1 )
-+        {
-+            String search = args[0].toLowerCase();
-+            for ( String player : BungeeMain.getOnlinePlayers() )
-+            {
-+                if ( player.toLowerCase().startsWith( search ) )
-+                {
-+                    matches.add( player );
-+                }
-+            }
-+        }
+        if (strings.length == 1) {
+            String search = strings[0].toLowerCase();
+            for (String player : BungeeMain.get().getOnlinePlayers()) {
+                if (player.toLowerCase().startsWith(search)){
+                    matches.add(player);
+                }
+            }
+        }
         return matches;
     }
 }
