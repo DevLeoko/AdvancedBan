a4da7d3bfde21ac49341e05fbb5345f1f9b06bcf
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-07-09 22:02:23 -0400

    Added a better tab completer for nicks

--- src/main/java/me/leoko/advancedban/bungee/listener/CommandReceiverBungee.java
+++ src/main/java/me/leoko/advancedban/bungee/listener/CommandReceiverBungee.java
@@ -1,6 +1,5 @@
 package me.leoko.advancedban.bungee.listener;
 
-import me.leoko.advancedban.bungee.BungeeMain;
 import me.leoko.advancedban.manager.CommandManager;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Command;
@@ -9,7 +8,8 @@ import net.md_5.bungee.api.plugin.TabExecutor;
 /**
  * Created by Leoko @ dev.skamps.eu on 24.07.2016.
  */
-public class CommandReceiverBungee extends Command implements TabExecutor {
+public class CommandReceiverBungee extends Command {
+
     public CommandReceiverBungee(String name) {
         super(name);
     }
@@ -17,10 +17,5 @@ public class CommandReceiverBungee extends Command implements TabExecutor {
     @Override
     public void execute(CommandSender sender, String[] args) {
         CommandManager.get().onCommand(sender, getName(), args);
-    }
-
-    @Override
-    public Iterable<String> onTabComplete(CommandSender commandSender, String[] strings) {
-        return BungeeMain.get().getOnlinePlayers();
     }
 }
