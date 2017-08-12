2a8c60b983655284f05d2cf4f1e789991b5e62ea
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-08-12 11:31:02 -0400

    [+] If you use redis it is shown in the /advancedban command

--- src/main/java/me/leoko/advancedban/manager/CommandManager.java
+++ src/main/java/me/leoko/advancedban/manager/CommandManager.java
@@ -328,6 +328,9 @@ public class CommandManager {
                     mi.sendMessage(sender, "  §cLicense §8• §7Public");
                     mi.sendMessage(sender, "  §cStorage §8• §7" + (DatabaseManager.get().isUseMySQL() ? "MySQL (external)" : "HSQLDB (local)"));
                     mi.sendMessage(sender, "  §cServer §8• §7" + (Universal.get().isBungee() ? "Bungeecord" : "Spigot/Bukkit"));
+                    if (Universal.get().isBungee()) {
+                        mi.sendMessage("  §cRedisBungee §8• §7", String.valueOf(Universal.get().useRedis()));
+                    }
                     mi.sendMessage(sender, "  §cUUID-Mode §8• §7" + UUIDManager.get().getMode());
                     mi.sendMessage(sender, "  §cPrefix §8• §7" + MessageManager.getMessage("General.Prefix"));
                     mi.sendMessage(sender, "§8§l§m-=========================-§r ");
