0645b3048ee6f2b1fdaec83b3dc8da1a2180fbe0
STG-Allen <ajacob715@gmail.com>
2021-02-24 23:00:44 -0500

    Ensure the ChatListenerVelocity recieves the event early

--- velocity/src/main/java/me/leoko/advancedban/velocity/listener/ChatListenerVelocity.java
+++ velocity/src/main/java/me/leoko/advancedban/velocity/listener/ChatListenerVelocity.java
@@ -1,12 +1,13 @@
 package me.leoko.advancedban.velocity.listener;
 
+import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.PlayerChatEvent;
 import me.leoko.advancedban.Universal;
 
 public class ChatListenerVelocity {
 
-  @Subscribe
+  @Subscribe(order = PostOrder.FIRST)
   public void onChat(PlayerChatEvent event) {
     if (!event.getMessage().startsWith("/")) {
       if (Universal.get().getMethods().callChat(event.getPlayer())) {
