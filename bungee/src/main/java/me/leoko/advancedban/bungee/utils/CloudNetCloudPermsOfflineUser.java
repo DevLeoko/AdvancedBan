4f5ade9b9f3b0074f037aaa8cafdbbfb5f4b69a4
Arnaud Lier <zeprofdecoding@gmail.com>
2021-02-26 12:07:03 +0100

    fix: check that the index is valid

--- bungee/src/main/java/me/leoko/advancedban/bungee/utils/CloudNetCloudPermsOfflineUser.java
+++ bungee/src/main/java/me/leoko/advancedban/bungee/utils/CloudNetCloudPermsOfflineUser.java
@@ -4,13 +4,20 @@ import me.leoko.advancedban.utils.Permissionable;
 import de.dytanic.cloudnet.driver.CloudNetDriver;
 import de.dytanic.cloudnet.driver.permission.IPermissionUser;
 
+import java.util.List;
 import java.util.UUID;
 
 public class CloudNetCloudPermsOfflineUser implements Permissionable {
     private IPermissionUser permissionUser;
 
     public CloudNetCloudPermsOfflineUser(String name) {
-        this.permissionUser = CloudNetDriver.getInstance().getPermissionManagement().getUsers(name).get(0);
+    	final List <IPermissionUser> users = CloudNetDriver.getInstance().getPermissionManagement().getUsers(name);
+    	
+    	if (!users.isEmpty()) {
+        	this.permissionUser = users.get(0);
+    	} else {
+    		this.permissionUser = null;
+    	}
     }
 
     @Override
