c4158326247ef14550a2d8429bd2ad8d131b1b20
Aurel Ballin <ballinaurel@gmail.com>
2022-08-27 10:45:37 +0000

    use static version of the nullTabCompleter instead

--- core/src/main/java/me/leoko/advancedban/utils/Command.java
+++ core/src/main/java/me/leoko/advancedban/utils/Command.java
@@ -531,6 +531,7 @@ public enum Command {
     private final Consumer<CommandInput> commandHandler;
     private final String usagePath;
     private final String[] names;
+    private static final NullTabCompleter nullTabCompleter = new NullTabCompleter();
 
     Command(String permission, Predicate<String[]> syntaxValidator,
             TabCompleter tabCompleter, Consumer<CommandInput> commandHandler, String usagePath, String... names) {
@@ -560,7 +561,7 @@ public enum Command {
     public TabCompleter getTabCompleter() {
         MethodInterface mi = Universal.get().getMethods();
         if (!mi.getBoolean(mi.getConfig(), "Use Tab Completion", true)) 
-            return new NullTabCompleter();
+            return nullTabCompleter;
         return tabCompleter;
     }
 
